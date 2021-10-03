9bfc1ae4373971491b66705981774eedbb960093
Gabriela Ribeiro <g.sribeiro@hotmail.com>
2021-10-03 20:15:41 -0300

    Add ngx-mask e novos estilos

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppComponent } from './app.component';
 import { EmployeeService } from './employee.service';
 import { HttpClientModule } from '@angular/common/http';import { FormsModule } from '@angular/forms';
 
+import { NgxMaskModule, IConfig } from 'ngx-mask'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule, FormsModule
+    HttpClientModule, 
+    FormsModule,
+    NgxMaskModule.forRoot({
+      dropSpecialCharacters: false // ao salvar, mantem a máscara
+    })
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
